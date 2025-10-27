3592e35744cde4d577205b71555c448a55e2df12
Ayas <my4hya@gmail.com>
2025-10-27 09:16:08 +0700

    spk card

--- index.php
+++ index.php
@@ -40,6 +40,12 @@ include 'config.php';
                 </div>
             </a>
 
+            <a href="daftar_spk.php" class="block bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow">
+                <div class="text-center">
+                    <i class="fa-solid fa-file-lines fa-5x mx-auto mb-4 text-blue-600"></i>
+                    <h2 class="text-xl font-semibold mb-2">daftar SPK</h2>
+                </div>
+            </a>
         </div>
         <?php else: ?>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
